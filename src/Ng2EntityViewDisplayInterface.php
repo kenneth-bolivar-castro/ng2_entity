c2ac580cf8214c54652c7756c161d5c9bdda4db6
Kenneth Bolivar <keboca@gmail.com>
2016-06-30 05:56:35 +0000

    Updated Ng2EntityViewDisplayInterface

--- src/Ng2EntityViewDisplayInterface.php
+++ src/Ng2EntityViewDisplayInterface.php
@@ -1,17 +1,20 @@
 <?php
 
-/**
- * @file Contains Ng2EntityViewDisplayInterface interface.
- */
-
 namespace Drupal\ng2_entity;
 
-
+/**
+ * Interface Ng2EntityViewDisplayInterface.
+ *
+ * @package Drupal\ng2_entity
+ */
 interface Ng2EntityViewDisplayInterface {
 
   /**
    * Retrieve component based on machine name.
-   * @param $machine_name string Given machine name.
+   *
+   * @param string $machine_name
+   *   Given machine name.
+   *
    * @return array
    *   Component definition.
    */
@@ -19,15 +22,22 @@ interface Ng2EntityViewDisplayInterface {
 
   /**
    * Create EntityViewModes based on given entity types.
-   * @param array $types Given entity types to create entity view.
-   * @param bool $show_message Check if message is required.
+   *
+   * @param array $types
+   *   Given entity types to create entity view.
+   * @param bool $show_message
+   *   Check if message is required.
    */
   public function createEntityViewModes(array $types, $show_message = FALSE);
 
   /**
    * Remove EntityViewModes based on given entity types.
-   * @param array $types Given entity types to remove entity view.
-   * @param bool $show_message Check if message is required.
+   *
+   * @param array $types
+   *   Given entity types to remove entity view.
+   * @param bool $show_message
+   *   Check if message is required.
    */
   public function removeEntityViewModes(array $types, $show_message = FALSE);
+
 }
